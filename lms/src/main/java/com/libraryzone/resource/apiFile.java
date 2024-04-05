661d7c847611e5bb4554d6beab102444cf7ff11f
sumanth <iamsumanth01@gmail.com>
2024-04-05 23:14:14 +0530

    creating a repository with initial project

---
+++ lms/src/main/java/com/libraryzone/resource/apiFile.java
@@ -0,0 +1,20 @@
+package com.libraryzone.resource;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class apiFile {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @GetMapping("/check")
+    public String getString() {
+        String responseString = "running properly";
+        return responseString;
+    }
+
+}
